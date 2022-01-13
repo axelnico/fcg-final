1de3c421495212cf55e723149f9ad355647ce8a4
Nicolas Bukovits <nicobuk@gmail.com>
2022-01-13 00:02:29 -0300

    primer intento de shader para dia y noche

--- src/client/planets.ts
+++ src/client/planets.ts
@@ -60,3 +60,77 @@ export const createPlanet = (name: string, position: number, size: number, surfa
     planetMesh.userData["rotationSpeed"] = rotationSpeed;
     return planetMesh;
 }
+
+const vs = `
+uniform vec3 sunDirection;
+
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 surfaceToLight;
+
+void main() {
+  vUv = uv;
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  vNormal = normalMatrix * normal;
+  vec3 surfaceWorldPosition = (modelMatrix * vec4(position,1.0)).xyz;
+  surfaceToLight = sunDirection - surfaceWorldPosition;
+  gl_Position = projectionMatrix * mvPosition;
+}
+`;
+
+const fs = `
+uniform sampler2D dayTexture;
+uniform sampler2D nightTexture;
+
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 surfaceToLight;
+
+void main( void ) {
+  vec3 dayColor = texture2D( dayTexture, vUv ).rgb;
+  vec3 nightColor = texture2D( nightTexture, vUv ).rgb;
+  vec3 surfaceToLightDirection = normalize(surfaceToLight);
+
+  // compute cosine sun to normal so -1 is away from sun and +1 is toward sun.
+  float cosineAngleSunToNormal = dot(normalize(vNormal), surfaceToLightDirection);
+
+  float light = dot(normalize(vNormal),surfaceToLightDirection);
+
+  // sharpen the edge beween the transition
+  cosineAngleSunToNormal = clamp( cosineAngleSunToNormal * 10.0, -1.0, 1.0);
+
+  // convert to 0 to 1 for mixing
+  float mixAmount = cosineAngleSunToNormal * 0.5 + 0.5;
+
+  // Select day or night texture based on mix.
+  vec3 color = mix( nightColor, dayColor, mixAmount );
+
+  //gl_FragColor = vec4( color, 1.0 );
+
+  gl_FragColor.rgb = dayColor; 
+
+ gl_FragColor.rgb *= light;
+}
+
+`;
+
+export const createEarth = (name: string, position: number, size: number, surface: string, rotationSpeed: number,scene: THREE.Scene) => {
+    //const texture = new THREE.TextureLoader().load(surface);
+    console.log(scene.getObjectByName("Sun")?.position)
+    const planetMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            sunDirection: {value: scene.getObjectByName("Sun")?.position},
+            dayTexture: { value: new THREE.TextureLoader().load( "2k_earth_daymap.jpeg" ) },
+            nightTexture: { value: new THREE.TextureLoader().load( "2k_earth_nightmap.jpeg" ) }
+        },
+        vertexShader: vs,
+        fragmentShader: fs
+    })
+    const planetMesh = new THREE.Mesh(sphereGeometry,planetMaterial);
+    planetMesh.name = name;
+    planetMesh.type = "planet";
+    planetMesh.position.x = position;
+    planetMesh.scale.set(size,size,size);
+    planetMesh.userData["rotationSpeed"] = rotationSpeed;
+    return planetMesh;
+}
